06bc12ffcf50851df836960c19760c05aaed2d82
Belster12 <kripa.Vasilyev@gmail.com>
2024-09-27 12:36:20 +0200

    s

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -10,7 +10,7 @@ const App = () => {
   const [currencyData, setCurrencyData] = useState(null);
 
   const fetchCurrencies = () => {
-    axios.get('http://fullstack-crypto-back.onrender.com/cryptocurrencies')
+    axios.get('https://fullstack-crypto-back.onrender.com/cryptocurrencies')
       .then(response => {
         const currenciesResponse = response.data; 
         const menuitem = [
@@ -31,7 +31,7 @@ const App = () => {
   };
 
   const fetchCurrency = () => {
-    axios.get(`http://fullstack-crypto-back.onrender.com/cryptocurrencies/${currencyId}`)
+    axios.get(`https://fullstack-crypto-back.onrender.com/cryptocurrencies/${currencyId}`)
       .then(response => {
         setCurrencyData(response.data);  // исправлено: r.data на response.data
       })
