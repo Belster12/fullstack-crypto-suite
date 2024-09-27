a191a06b9d2725aa70e39bb22e9e7dec2e9440e7
Belster12 <kripa.Vasilyev@gmail.com>
2024-09-27 11:59:50 +0200

    vs full url

--- backend/src/main.py
+++ backend/src/main.py
@@ -5,8 +5,6 @@ from fastapi.middleware.cors import CORSMiddleware
 app = FastAPI()
 
 origins = [
-    "http://localhost:5173",
-    "http://127.0.0.1:5173",
     "https://fullstack-crypto-suite.onrender.com",
 ]
 
